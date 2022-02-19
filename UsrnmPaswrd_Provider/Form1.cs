ff5e8b2dc7041874407591f8c3b4002a598bee1e
saurabhbaruas <saurabhrwt27@gmail.com>
2022-02-19 21:53:01 +0530

    First commit

---
+++ UsrnmPaswrd_Provider/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace UsrnmPaswrd_Provider
+{
+    public partial class Form1 : Form
+    {
+        string str = @"Data Source=LAPTOP-2OU8NEFO\SQLEXPRESS;Initial Catalog=LoginProvider;Integrated Security=True;";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void insert()
+        {
+            try
+            {
+                SqlConnection ssr = new SqlConnection(str);
+                ssr.Open();
+                SqlCommand cmd = new SqlCommand("insert into stdInfo (sName,FatherNmae,RollNo,DOB,Course,UserName,Password)values(@Name,@FatherName,@RollNo, @DOB,@Course,@UserName,@Password)", ssr);
+                cmd.Parameters.AddWithValue("@Name", textBox1.Text);
+                cmd.Parameters.AddWithValue("@FatherName", textBox2.Text);
+                cmd.Parameters.AddWithValue("@RollNo", textBox3.Text);
+                cmd.Parameters.AddWithValue("@DOB", dateTimePicker1.Text);
+                cmd.Parameters.AddWithValue("@Course", comboBox1.Text);
+                cmd.Parameters.AddWithValue("@UserName", textBox4.Text);
+                cmd.Parameters.AddWithValue("@Password", textBox5.Text);
+                cmd.ExecuteNonQuery();
+                ssr.Close();
+            }catch(Exception ex)
+            {
+                MessageBox.Show("Error"+ex);
+            }
+
+        }
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void stdInfo_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            insert();
+        }
+    }
+}
---
+++ UsrnmPaswrd_Provider/Login.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UsrnmPaswrd_Provider
+{
+    public partial class Login : Form
+    {
+        string str = @"Data Source=LAPTOP-2OU8NEFO\SQLEXPRESS;Initial Catalog=LoginProvider;Integrated Security=True;";
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection ssr = new SqlConnection(str);
+            ssr.Open();
+            SqlCommand cmd = new SqlCommand("Select * From stdInfo where UserName ='" + textBox1.Text + "' and Password='" + textBox2.Text + "'", ssr);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            if (ds.Tables[0].Rows.Count > 0)
+            {
+                label4.Text = "sucess";
+                Form1 fm = new Form1();
+                fm.Show();
+                this.Hide();
+            }
+            else
+            {
+                label4.Text = "Failure";
+            }
+            ssr.Close();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
